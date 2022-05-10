e8ad0d4d94d646781a000ae0c5ae36c8474d4526
LukasKratochvila <kratochvila.lukas@gmail.com>
2022-05-10 14:06:34 +0200

    Update clustering
    Added orientation of BBox

--- src/clustering/src/clustering.cpp
+++ src/clustering/src/clustering.cpp
@@ -11,9 +11,6 @@
 #include <pcl/filters/extract_indices.h>
 #include <pcl/filters/statistical_outlier_removal.h>
 
-
-
-
 #include <pcl/filters/voxel_grid.h>
 
 #include <pcl/kdtree/kdtree.h>
@@ -28,210 +25,299 @@
 #include <vision_msgs/msg/detection3_d.hpp>
 #include <vision_msgs/msg/detection3_d_array.hpp>
 
+#include <pcl/features/moment_of_inertia_estimation.h>
+
+#include <pcl/common/transforms.h>
+
 class Clustering : public rclcpp::Node
 {
-  public:
-    Clustering() : Node("clustering")
-    {
-      this->declare_parameter("input_topic", "/filteredPcl");
-      this->declare_parameter("output_topic", "/detections_det");
-      this->declare_parameter("output_topic_vizualization", "/detections");
-      this->declare_parameter("cluster_tolerance", 0.2);
-      this->declare_parameter("min_cluster_size", 20);
-      this->declare_parameter("max_cluster_size", 10000);
-      this->declare_parameter("debug", false);
-      
-      input_topic = this->get_parameter("input_topic").as_string();
-      output_topic = this->get_parameter("output_topic").as_string();
-      output_topic_vizualization = this->get_parameter("output_topic_vizualization").as_string();
-      cluster_tolerance = this->get_parameter("cluster_tolerance").as_double();
-      min_cluster_size = this->get_parameter("min_cluster_size").as_int();
-      max_cluster_size = this->get_parameter("max_cluster_size").as_int();
-      debug = this->get_parameter("debug").as_bool();
-      
-      subscription_ = this->create_subscription<sensor_msgs::msg::PointCloud2>(input_topic, 10, std::bind(&Clustering::topic_callback, this, std::placeholders::_1));
-      marker_publisher_ = this->create_publisher<visualization_msgs::msg::MarkerArray>(output_topic_vizualization, 10);
-      detections_publisher_ = this->create_publisher<vision_msgs::msg::Detection3DArray>(output_topic, 10);
+public:
+  Clustering() : Node("clustering")
+  {
+    this->declare_parameter("input_topic", "/filteredPcl");
+    this->declare_parameter("output_topic", "/detections_det");
+    this->declare_parameter("cluster_tolerance", 0.2);
+    this->declare_parameter("min_cluster_size", 20);
+    this->declare_parameter("max_cluster_size", 10000);
+    this->declare_parameter("OBB", false);
+    this->declare_parameter("step", 15);
+    this->declare_parameter("debug", false);
 
-      RCLCPP_INFO(this->get_logger(), "Clustering node has started.");
+    input_topic = this->get_parameter("input_topic").as_string();
+    output_topic = this->get_parameter("output_topic").as_string();
+    cluster_tolerance = this->get_parameter("cluster_tolerance").as_double();
+    min_cluster_size = this->get_parameter("min_cluster_size").as_int();
+    max_cluster_size = this->get_parameter("max_cluster_size").as_int();
+    get_parameter("OBB", OBB);
+    get_parameter("step", step);
+    debug = this->get_parameter("debug").as_bool();
+
+    subscription_ = this->create_subscription<sensor_msgs::msg::PointCloud2>(input_topic, 10, std::bind(&Clustering::topic_callback, this, std::placeholders::_1));
+    detections_publisher_ = this->create_publisher<vision_msgs::msg::Detection3DArray>(output_topic, 10);
+
+    RCLCPP_INFO(this->get_logger(), "Clustering node has started.");
+  }
+
+private:
+  void Get_AABB_(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_ptr, pcl::Indices *indices, pcl::PointXYZ *center_point, pcl::PointXYZ *min_point, pcl::PointXYZ *max_point) const
+	{// https://pointclouds.org/documentation/moment__of__inertia__estimation_8hpp_source.html
+    center_point->x = 0.0f;
+    center_point->y = 0.0f;
+    center_point->z = 0.0f;
+    
+    min_point->x = std::numeric_limits<float>::max();
+    min_point->y = std::numeric_limits<float>::max();
+    min_point->z = std::numeric_limits<float>::max();
+    
+    max_point->x = -std::numeric_limits<float>::max();
+    max_point->y = -std::numeric_limits<float>::max();
+    max_point->z = -std::numeric_limits<float>::max();
+    
+    unsigned int number_of_points = static_cast <unsigned int>(indices->size());
+    for (unsigned int i_point = 0; i_point < number_of_points; i_point++)
+    {
+      center_point->x += (*cloud_ptr)[(*indices)[i_point]].x;
+      center_point->y += (*cloud_ptr)[(*indices)[i_point]].y;
+      center_point->z += (*cloud_ptr)[(*indices)[i_point]].z;
+    
+      if ((*cloud_ptr)[(*indices)[i_point]].x <= min_point->x) min_point->x = (*cloud_ptr)[(*indices)[i_point]].x;
+      if ((*cloud_ptr)[(*indices)[i_point]].y <= min_point->y) min_point->y = (*cloud_ptr)[(*indices)[i_point]].y;
+      if ((*cloud_ptr)[(*indices)[i_point]].z <= min_point->z) min_point->z = (*cloud_ptr)[(*indices)[i_point]].z;
+    
+      if ((*cloud_ptr)[(*indices)[i_point]].x >= max_point->x) max_point->x = (*cloud_ptr)[(*indices)[i_point]].x;
+      if ((*cloud_ptr)[(*indices)[i_point]].y >= max_point->y) max_point->y = (*cloud_ptr)[(*indices)[i_point]].y;
+      if ((*cloud_ptr)[(*indices)[i_point]].z >= max_point->z) max_point->z = (*cloud_ptr)[(*indices)[i_point]].z;
+    }
+    
+    if (number_of_points == 0)
+      number_of_points = 1;
+    
+    center_point->x /= number_of_points;
+    center_point->y /= number_of_points;
+    center_point->z /= number_of_points;
+  }
+  void topic_callback(const sensor_msgs::msg::PointCloud2::SharedPtr msg) const
+  {
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
+    pcl::fromROSMsg(*msg, *cloud);
+
+    // Dont have anything to process -> return
+    if (!(*cloud).size())
+      return;
+
+    std::stringstream log;
+    // Creating the KdTree object for the search method of the extraction
+    pcl::search::KdTree<pcl::PointXYZ>::Ptr tree(new pcl::search::KdTree<pcl::PointXYZ>);
+    tree->setInputCloud(cloud->makeShared());
+
+    std::vector<pcl::PointIndices> cluster_indices;
+    pcl::EuclideanClusterExtraction<pcl::PointXYZ> ec;
+    ec.setClusterTolerance(cluster_tolerance);
+    ec.setMinClusterSize(min_cluster_size);
+    ec.setMaxClusterSize(max_cluster_size);
+    ec.setSearchMethod(tree);
+    ec.setInputCloud(cloud->makeShared());
+    ec.extract(cluster_indices);
+
+    if (debug)
+    {
+      log << "PointCloud representing: " << cluster_indices.size() << " Clusters." << std::endl;
+      RCLCPP_INFO(this->get_logger(), log.str());
+      log.clear();
     }
 
-  private:
-    void topic_callback(const sensor_msgs::msg::PointCloud2::SharedPtr msg) const
+    // Dont have anything to publish -> return
+    if (!cluster_indices.size())
+      return;
+
+    std::vector<double> cx, cy, cz, dx, dy, dz, ox, oy, oz, ow;
+
+    if (!OBB)
     {
-      pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
-      pcl::fromROSMsg(*msg,*cloud);
-
-      std::stringstream log;
-      // Creating the KdTree object for the search method of the extraction
-      pcl::search::KdTree<pcl::PointXYZ>::Ptr tree(new pcl::search::KdTree<pcl::PointXYZ>);
-      tree->setInputCloud(cloud->makeShared());
-      
-      std::vector<pcl::PointIndices> cluster_indices;
-      pcl::EuclideanClusterExtraction<pcl::PointXYZ> ec;
-      ec.setClusterTolerance(cluster_tolerance);
-      ec.setMinClusterSize(min_cluster_size);
-      ec.setMaxClusterSize(max_cluster_size);
-      ec.setSearchMethod(tree);
-      ec.setInputCloud(cloud->makeShared());
-      ec.extract(cluster_indices);
-
-      if (debug){
-        log << "PointCloud representing: " << cluster_indices.size () << " Clusters.";
-        RCLCPP_INFO(this->get_logger(), log.str());
-        log.clear();
-      }
-
-      std::vector<pcl::PointXYZ> min_list;
-      std::vector<pcl::PointXYZ> max_list;
-
-      for (auto& it : cluster_indices)
+      std::vector<pcl::PointXYZ> center_list, size_list;
+      for (auto &it : cluster_indices)
       {
-        pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_cluster (new pcl::PointCloud<pcl::PointXYZ>);
-
+        pcl::PointXYZ centerPoint = {NAN, NAN, NAN};
         pcl::PointXYZ minPoint = {NAN, NAN, NAN};
         pcl::PointXYZ maxPoint = {NAN, NAN, NAN};
 
-        for (auto pit = it.indices.begin()+1 ; pit < it.indices.end() ; pit++) { // dont know wky, but sometimes point at index [0] is {0,0,0}
-            const auto& point = cloud->makeShared()->points[*pit];
-
-            if (*pit != it.indices[3]) {
-                if(minPoint.x > point.x) {minPoint.x = point.x;}
-                if(minPoint.y > point.y) {minPoint.y = point.y;}
-                if(minPoint.z > point.z) {minPoint.z = point.z;}
-                if(maxPoint.x < point.x) {maxPoint.x = point.x;}
-                if(maxPoint.y < point.y) {maxPoint.y = point.y;}
-                if(maxPoint.z < point.z) {maxPoint.z = point.z;}
-            } else {
-                minPoint.x = point.x;minPoint.y = point.y;minPoint.z = point.z;
-                maxPoint.x = point.x;maxPoint.y = point.y;maxPoint.z = point.z;
-            }
-        }
-        min_list.emplace_back(minPoint);
-        max_list.emplace_back(maxPoint);
+        Get_AABB_(cloud, &it.indices, &centerPoint, &minPoint, &maxPoint);
+        center_list.emplace_back(centerPoint);
+        maxPoint.x -= minPoint.x;
+        maxPoint.y -= minPoint.y;
+        maxPoint.z -= minPoint.z;
+        size_list.emplace_back(maxPoint);
       }
-
-      visualization_msgs::msg::MarkerArray output;
-      vision_msgs::msg::Detection3DArray output_det;
-
-      //auto timestamp = builtin_interfaces::msg::Time();
-      //auto frame = msg->header.frame_id;
-
-      size_t cnt = 0;
-
-      for( size_t i = 0; i < min_list.size(); i++) {
-        double dx = max_list.at(i).x - min_list.at(i).x;
-        double dy = max_list.at(i).y - min_list.at(i).y;
-        double dz = max_list.at(i).z - min_list.at(i).z;
-
-        if (dx == 0 || dy == 0 || dz == 0)
-          continue;
-
-        double cx = (max_list.at(i).x + min_list.at(i).x) / 2;
-        double cy = (max_list.at(i).y + min_list.at(i).y) / 2;
-        double cz = (max_list.at(i).z + min_list.at(i).z) / 2;
-
-        // Bounding Box
-        visualization_msgs::msg::Marker marker;
-        //marker.header.frame_id = frame;
-        //marker.header.stamp = timestamp;
-        marker.header = msg->header;
-        marker.id = cnt++;
-        marker.type = visualization_msgs::msg::Marker::CUBE;
-        marker.action = visualization_msgs::msg::Marker::ADD;
-
-        marker.pose.position.x = cx;
-        marker.pose.position.y = cy;
-        marker.pose.position.z = cz;
-
-        marker.pose.orientation.x = 0.0;
-        marker.pose.orientation.y = 0.0;
-        marker.pose.orientation.z = 0.0;
-        marker.pose.orientation.w = 1.0;
-
-        marker.scale.x = dx;
-        marker.scale.y = dy;
-        marker.scale.z = dz;
-
-        marker.color.a = 0.6;
-        marker.color.r = 0.7;
-        marker.color.g = 0.7;
-        marker.color.b = 0.7;
-
-        output.markers.push_back(marker);
-
-        visualization_msgs::msg::Marker text;
-        //text.header.frame_id = frame;
-        //text.header.stamp = timestamp;
-        text.header = msg->header;
-        text.id = cnt++;
-        text.type = visualization_msgs::msg::Marker::TEXT_VIEW_FACING;
-        text.action = visualization_msgs::msg::Marker::ADD;
-
-        std::stringstream sstream;
-        sstream << "ID: " << i;// << std::endl;
-                //<< "TTL: " << detection->getTTL() << std::endl;
-        text.text = sstream.str();
-
-        text.pose.position.x = max_list.at(i).x;
-        text.pose.position.y = max_list.at(i).y;
-        text.pose.position.z = max_list.at(i).z;
-
-        text.scale.x = 1;
-        text.scale.y = 1;
-        text.scale.z = 1;
-
-        text.color.a = 0.6;
-        text.color.r = 0.7;
-        text.color.g = 0.7;
-        text.color.b = 0.7;
-
-        output.markers.push_back(text);
-        
-        vision_msgs::msg::Detection3D det;
-        //det.header.frame_id = frame;
-        //det.header.stamp = timestamp;
-        det.header = msg->header;
-        det.tracking_id = std::to_string(cnt/2);
-
-        det.bbox.center.position.x = cx;
-        det.bbox.center.position.y = cy;
-        det.bbox.center.position.z = cz;
-
-        det.bbox.center.orientation.x = 0.0;
-        det.bbox.center.orientation.y = 0.0;
-        det.bbox.center.orientation.z = 0.0;
-        det.bbox.center.orientation.w = 1.0;
-
-        det.bbox.size.x = dx;
-        det.bbox.size.y = dy;
-        det.bbox.size.z = dz;
-
-        output_det.detections.push_back(det);
+      for (size_t i = 0; i < center_list.size(); i++)
+      {
+        cx.emplace_back(center_list.at(i).x);
+        cy.emplace_back(center_list.at(i).y);
+        cz.emplace_back(center_list.at(i).z);
+        dx.emplace_back(size_list.at(i).x);
+        dy.emplace_back(size_list.at(i).y);
+        dz.emplace_back(size_list.at(i).z);
+        ox.emplace_back(0.0);
+        oy.emplace_back(0.0);
+        oz.emplace_back(0.0);
+        ow.emplace_back(1.0);
       }
-      output_det.header = output_det.detections[0].header;
-      marker_publisher_->publish(output);
-      detections_publisher_->publish(output_det);
-
-      if (debug){
-        log << "Publishing: " << min_list.size() << " detections.";
+    }
+    else
+    {
+      if (debug)
+      {
+        log << "Computing clusters OBB." << std::endl;
         RCLCPP_INFO(this->get_logger(), log.str());
         log.clear();
       }
+      pcl::PointXYZ centerPoint, minPoint, maxPoint, center_point, min_point, max_point;
+      
+      for (pcl::PointIndices &it : cluster_indices)
+      {
+        pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_cluster(new pcl::PointCloud<pcl::PointXYZ>);
+        pcl::Indices cluster_idx;
+        for (const auto &idx : it.indices)
+          cloud_cluster->push_back((*cloud)[idx]);
+        for (size_t i=0; i<cloud_cluster->size(); i++)
+          cluster_idx.push_back(i);
+
+        Clustering::Get_AABB_(cloud_cluster, &cluster_idx, &centerPoint, &minPoint, &maxPoint);
+
+        double angle = 0;
+        double surface_area = (maxPoint.x - minPoint.x) * (maxPoint.y - minPoint.y);
+        min_point = minPoint;
+        max_point = maxPoint;
+        center_point = centerPoint;
+
+        for(size_t angle_step=1; angle_step*step < 180; angle_step++){
+          Eigen::Matrix4f transform_rot = Eigen::Matrix4f::Identity();
+          transform_rot.topLeftCorner(3, 3) = Eigen::Matrix3f(Eigen::Quaternionf(cos(angle_step*step/2),sin(angle_step*step/2),0,0));
+				  pcl::transformPointCloud(*cloud_cluster, *cloud_cluster, transform_rot);
+
+          Clustering::Get_AABB_(cloud_cluster, &cluster_idx, &centerPoint, &minPoint, &maxPoint);
+          if ((maxPoint.x - minPoint.x) * (maxPoint.y - minPoint.y) < surface_area){
+            surface_area = (maxPoint.x - minPoint.x) * (maxPoint.y - minPoint.y);
+            angle = angle_step*step;
+            min_point = minPoint;
+            max_point = maxPoint;
+            center_point = centerPoint;
+          }
+        }
+
+        cx.emplace_back(center_point.x);
+        cy.emplace_back(center_point.y); 
+        cz.emplace_back(center_point.z); 
+        dx.emplace_back(max_point.x - min_point.x);
+        dy.emplace_back(max_point.y - min_point.y); 
+        dz.emplace_back(max_point.z - min_point.z); 
+        ox.emplace_back(cos(angle/2));
+        oy.emplace_back(sin(angle/2));
+        oz.emplace_back(0);
+        ow.emplace_back(0);
+      }
     }
-    rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr subscription_{nullptr};
-    rclcpp::Publisher<visualization_msgs::msg::MarkerArray>::SharedPtr marker_publisher_{nullptr};
-    rclcpp::Publisher<vision_msgs::msg::Detection3DArray>::SharedPtr detections_publisher_{nullptr};
-    
-    std::string input_topic, output_topic, output_topic_vizualization;
-    double cluster_tolerance, min_cluster_size, max_cluster_size;
-    bool debug;
+
+    if (debug)
+    {
+      log << "Creating Detection3DArray..." << std::endl;
+      RCLCPP_INFO(this->get_logger(), log.str());
+      log.clear();
+    }
+
+    vision_msgs::msg::Detection3DArray output_det;
+    size_t cnt = 0;
+    for (size_t i = 0; i < cx.size(); i++)
+    {
+      vision_msgs::msg::Detection3D det;
+      det.header = msg->header;
+      det.tracking_id = std::to_string(cnt++);
+
+      det.bbox.center.position.x = cx.at(i);
+      det.bbox.center.position.y = cy.at(i);
+      det.bbox.center.position.z = cz.at(i);
+
+      det.bbox.center.orientation.x = ox.at(i);
+      det.bbox.center.orientation.y = oy.at(i);
+      det.bbox.center.orientation.z = oz.at(i);
+      det.bbox.center.orientation.w = ow.at(i);
+
+      det.bbox.size.x = dx.at(i);
+      det.bbox.size.y = dy.at(i);
+      det.bbox.size.z = dz.at(i);
+
+      output_det.detections.push_back(det);
+    }
+    output_det.header = output_det.detections[0].header;
+    detections_publisher_->publish(output_det);
+
+    if (debug)
+    {
+      log << "Publishing: " << cx.size() << " detections." << std::endl;
+      RCLCPP_INFO(this->get_logger(), log.str());
+      log.clear();
+    }
+  }
+  rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr subscription_{nullptr};
+  rclcpp::Publisher<vision_msgs::msg::Detection3DArray>::SharedPtr detections_publisher_{nullptr};
+
+  std::string input_topic, output_topic, output_topic_vizualization;
+  double cluster_tolerance, min_cluster_size, max_cluster_size;
+  bool OBB;
+  int step;
+  bool debug;
 };
 
-int main(int argc, char * argv[])
+int main(int argc, char *argv[])
 {
   rclcpp::init(argc, argv);
   rclcpp::spin(std::make_shared<Clustering>());
   rclcpp::shutdown();
   return 0;
 }
+
+/*
+pcl::PointXYZ min_point_AABB, max_point_AABB, min_point, max_point, centerPoint, minPoint, maxPoint;
+      Eigen::Vector3f mass_center;
+      bool is_valid;
+
+      pcl::MomentOfInertiaEstimation<pcl::PointXYZ> feature_extractor;
+      for (pcl::PointIndices &it : cluster_indices)
+      {
+        Clustering::Get_AABB_(cloud, &it.indices, &centerPoint, &minPoint, &maxPoint);
+        pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_cluster(new pcl::PointCloud<pcl::PointXYZ>);
+        for (const auto &idx : it.indices)
+          cloud_cluster->push_back((*cloud)[idx]);
+
+        feature_extractor.setInputCloud(cloud_cluster);
+        feature_extractor.compute();
+
+        feature_extractor.getAABB(min_point_AABB, max_point_AABB);
+        is_valid = feature_extractor.getMassCenter(mass_center);
+        if (debug)
+        {
+          log << "Computed OBB:: is valid?: " << is_valid << std::endl
+              << "mass_center: " << mass_center << std::endl;
+          RCLCPP_INFO(this->get_logger(), log.str());
+          log.clear();
+        }
+        double angle = 0;
+        double surface_area = (max_point_AABB.x - min_point_AABB.x) * (max_point_AABB.y - min_point_AABB.y);
+        min_point = min_point_AABB;
+        max_point = max_point_AABB;
+
+        for(size_t angle_step=1; angle_step*step < 180; angle_step++){
+          Eigen::Matrix4f transform_rot = Eigen::Matrix4f::Identity();
+          transform_rot.topLeftCorner(3, 3) = Eigen::Matrix3f(Eigen::Quaternionf(cos(angle_step*step/2),sin(angle_step*step/2),0,0));
+				  pcl::transformPointCloud(*cloud_cluster, *cloud_cluster, transform_rot);
+
+          feature_extractor.setInputCloud(cloud_cluster);
+          feature_extractor.compute();
+          feature_extractor.getAABB(min_point_AABB, max_point_AABB);
+          if ((max_point_AABB.x - min_point_AABB.x) * (max_point_AABB.y - min_point_AABB.y) < surface_area){
+            surface_area = (max_point_AABB.x - min_point_AABB.x) * (max_point_AABB.y - min_point_AABB.y);
+            angle=angle_step*step;
+            min_point = min_point_AABB;
+            max_point = max_point_AABB;
+          }
+        }
+        */
