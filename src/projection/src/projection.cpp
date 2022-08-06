4c1b61d2bbd8124cc6a64f861705e4d7150b946b
LukasKratochvila <kratochvila.lukas@gmail.com>
2022-08-06 20:59:04 +0200

    Working version
    Rewrited to use 2D detection
    Calibrationparameters in config

--- src/projection/src/projection.cpp
+++ src/projection/src/projection.cpp
@@ -16,11 +16,9 @@
 #include "opencv2/calib3d/calib3d.hpp"
 #include <opencv2/imgcodecs.hpp>
 
-#include <visualization_msgs/msg/marker.hpp>
-#include <visualization_msgs/msg/marker_array.hpp>
-
 #include "sensor_msgs/msg/image.hpp"
 #include "vision_msgs/msg/detection2_d_array.hpp"
+#include <vision_msgs/msg/detection3_d_array.hpp>
 
 #include <cv_bridge/cv_bridge.h>
 //#include <sensor_msgs/image_encodings.h>
@@ -34,26 +32,33 @@ class Projection : public rclcpp::Node
     {
       this->declare_parameter("input_topic", "/detections");
       this->declare_parameter("output_topic", "/cluster_det");
-      this->declare_parameter("output_pcl_topic", "~/pcl");
-      this->declare_parameter("output_img_topic", "~/img");
+      this->declare_parameter("dbg_img_topic", "~/img");
       this->declare_parameter("output_frame", "image");
 
+      this->declare_parameter("K",  std::vector<double>({531.7944, 0, 296.3159, 0, 530.179, 255.4285, 0, 0, 1}));
+      this->declare_parameter("D", std::vector<double>({-0.4512, 0.7176, -0.0014, 0.0017, -1.2896}));
+
       this->declare_parameter("debug", false);
+      
       
       output_frame = this->get_parameter("output_frame").as_string();
       debug = this->get_parameter("debug").as_bool();
 
-      det_subscription_ = this->create_subscription<visualization_msgs::msg::MarkerArray>(
+      det_subscription_ = this->create_subscription<vision_msgs::msg::Detection3DArray>(
       this->get_parameter("input_topic").as_string(), 10, std::bind(&Projection::topic_callback, this, std::placeholders::_1));
       publisher_ = this->create_publisher<vision_msgs::msg::Detection2DArray>(this->get_parameter("output_topic").as_string(), 10);
       if (debug){
-        pcl_publisher_ = this->create_publisher<visualization_msgs::msg::MarkerArray>(this->get_parameter("output_pcl_topic").as_string(), 10);
-        img_publisher_ = this->create_publisher<sensor_msgs::msg::Image>(this->get_parameter("output_img_topic").as_string(), 10);
+        img_publisher_ = this->create_publisher<sensor_msgs::msg::Image>(this->get_parameter("dbg_img_topic").as_string(), 10);
       }
 
       tf_buffer_ = std::make_unique<tf2_ros::Buffer>(this->get_clock());
       transform_listener_ = std::make_shared<tf2_ros::TransformListener>(*tf_buffer_);
       
+      std::vector<double> pom_K = this->get_parameter("K").as_double_array();
+      std::vector<double> pom_D = this->get_parameter("D").as_double_array();
+      memcpy(K.data, pom_K.data(), pom_K.size()*sizeof(double));
+      memcpy(D.data, pom_D.data(), pom_D.size()*sizeof(double));
+
       std::stringstream ss;
       if (debug)
         ss << std::endl << "K = " << std::endl << K  << std::endl << "DistCoefs = " << std::endl << D << std::endl;
@@ -63,13 +68,13 @@ class Projection : public rclcpp::Node
     }
 
   private:
-    void topic_callback(const visualization_msgs::msg::MarkerArray::ConstSharedPtr detections) const
+    void topic_callback(const vision_msgs::msg::Detection3DArray::ConstSharedPtr detections) const
     {
-      if (detections->markers.size()== 0)
+      if (detections->detections.size()== 0)
       {
         return;
       } 
-      std::string fromFrameRel = detections->markers.at(0).header.frame_id;
+      std::string fromFrameRel = detections->detections.at(0).header.frame_id;
       std::string toFrameRel = output_frame;
       geometry_msgs::msg::TransformStamped transformStamped;
       try {
@@ -82,7 +87,7 @@ class Projection : public rclcpp::Node
       }
       std::stringstream ss;
       if (debug){
-        ss << detections->markers.size()/2 << std::endl;
+        ss << detections->detections.size() << std::endl;
         RCLCPP_INFO(get_logger(),ss.str());
         ss.clear();
       }
@@ -90,36 +95,31 @@ class Projection : public rclcpp::Node
       //tf2::Transform trans(transformStamped.transform.rotation);
       // Transform to image frame
       
-      visualization_msgs::msg::MarkerArray trans_det = visualization_msgs::msg::MarkerArray();
-      geometry_msgs::msg::PoseStamped marker_pose;
-      visualization_msgs::msg::Marker marker;
-      for(size_t i = 0; i < detections->markers.size(); i++) { 
-        marker = detections->markers.at(i);
-        marker_pose.header = marker.header;
-        marker_pose.pose = marker.pose;
+      vision_msgs::msg::Detection3DArray trans_det = vision_msgs::msg::Detection3DArray();
+      vision_msgs::msg::Detection3D det;
+      geometry_msgs::msg::PoseStamped pose;
+      for(size_t i = 0; i < detections->detections.size(); i++) { 
+        det = detections->detections.at(i);
+        pose.header=det.header;
+        pose.pose=det.bbox.center;
         //marker_pose = tf_buffer_->transform(marker_pose,"image");
-        tf2::doTransform(marker_pose,marker_pose,transformStamped);
-        marker.pose = marker_pose.pose;
-        trans_det.markers.push_back(marker);
+        tf2::doTransform(pose,pose,transformStamped);
+        det.bbox.center=pose.pose;
+        trans_det.detections.push_back(det);
       }
-      if (debug)
-        pcl_publisher_->publish(trans_det);
       
       cv::Mat rVec = (cv::Mat_<double>(3,1) << 0, 0, 0);
       cv::Mat tVec = (cv::Mat_<double>(3,1) << 0, 0, 0);
 
       std::vector<cv::Point3d> objectPoints;
       std::vector<cv::String> objectStrings;
-      for(size_t i = 0; i < trans_det.markers.size(); i++) { 
-        auto pt = trans_det.markers.at(i);
-        if (pt.type == visualization_msgs::msg::Marker::TEXT_VIEW_FACING){
-          objectStrings.push_back(cv::String(pt.text));
-          continue;
-        }
-        objectPoints.push_back(cv::Point3d(-pt.pose.position.y, -pt.pose.position.z, pt.pose.position.x));
-        objectPoints.push_back(cv::Point3d(-(pt.pose.position.y - pt.scale.y/2), -(pt.pose.position.z - pt.scale.z/2), (pt.pose.position.x - pt.scale.x/2)));
+      for(size_t i = 0; i < trans_det.detections.size(); i++) { 
+        auto pt = trans_det.detections.at(i);
+        objectStrings.push_back(pt.tracking_id);
+        objectPoints.push_back(cv::Point3d(-pt.bbox.center.position.y, -pt.bbox.center.position.z, pt.bbox.center.position.x));
+        objectPoints.push_back(cv::Point3d(-(pt.bbox.center.position.y - pt.bbox.size.y/2), -(pt.bbox.center.position.z - pt.bbox.size.z/2), (pt.bbox.center.position.x - pt.bbox.size.x/2)));
         //objectPoints.push_back(cv::Point3d(-(pt.pose.position.y + pt.scale.y/2), -(pt.pose.position.z - pt.scale.z/2), (pt.pose.position.x - pt.scale.x/2)));
-        objectPoints.push_back(cv::Point3d(-(pt.pose.position.y + pt.scale.y/2), -(pt.pose.position.z + pt.scale.z/2), (pt.pose.position.x - pt.scale.x/2)));
+        objectPoints.push_back(cv::Point3d(-(pt.bbox.center.position.y + pt.bbox.size.y/2), -(pt.bbox.center.position.z + pt.bbox.size.z/2), (pt.bbox.center.position.x - pt.bbox.size.x/2)));
         //objectPoints.push_back(cv::Point3d(-(pt.pose.position.y - pt.scale.y/2), -(pt.pose.position.z + pt.scale.z/2), (pt.pose.position.x - pt.scale.x/2)));
       }
       std::vector<cv::Point2d> projectedPoints;
@@ -132,13 +132,13 @@ class Projection : public rclcpp::Node
           cv::putText(img, objectStrings.at(i),cv::Point2d(projectedPoints.at(3*i).x,projectedPoints.at(3*i).y), cv::FONT_HERSHEY_SIMPLEX, 0.5, CV_RGB(0,255,0));
           cv::rectangle(img, cv::Rect2d(cv::Point2d(projectedPoints.at(3*i+1).x,projectedPoints.at(3*i+1).y),cv::Point2d(projectedPoints.at(3*i+2).x,projectedPoints.at(3*i+2).y)),CV_RGB(0,0,255));
         }
-        ss << projectedPoints;
+        //ss << projectedPoints;
         RCLCPP_INFO(get_logger(),ss.str());
         ss.clear();
 
         cv_bridge::CvImage out_msg;
         out_msg.image = img;
-        out_msg.header = trans_det.markers.at(0).header;
+        out_msg.header = trans_det.detections.at(0).header;
         out_msg.encoding = "bgr8";
         out_msg.header.frame_id = output_frame;
         //out_msg.header.stamp = detections->markers.at(0).header.stamp;
@@ -147,7 +147,7 @@ class Projection : public rclcpp::Node
       }
 
       vision_msgs::msg::Detection2DArray::UniquePtr detect(new vision_msgs::msg::Detection2DArray);
-      detect->header=trans_det.markers.at(0).header;
+      detect->header=trans_det.detections.at(0).header;
       detect->detections.reserve(objectStrings.size());
       for (size_t i = 0; i < objectStrings.size(); ++i) {
         detect->detections.emplace_back();
@@ -169,7 +169,7 @@ class Projection : public rclcpp::Node
         detection_ros.results.emplace_back();
         auto & dis = detection_ros.results.back();
         dis.id = "Distance";
-        dis.score = -sqrt(pow(trans_det.markers.at(2*i).pose.position.x,2)+pow(trans_det.markers.at(2*i).pose.position.y,2)+pow(trans_det.markers.at(2*i).pose.position.z,2));
+        dis.score = -sqrt(pow(trans_det.detections.at(i).bbox.center.position.x,2)+pow(trans_det.detections.at(i).bbox.center.position.y,2)+pow(trans_det.detections.at(i).bbox.center.position.z,2));
         
         // Copy bounding box, darknet uses center of bounding box too
         detection_ros.bbox.center.x = (projectedPoints.at(3*i+2).x+projectedPoints.at(3*i+1).x)/2;
@@ -179,15 +179,14 @@ class Projection : public rclcpp::Node
       }
       publisher_->publish(*detect);
     }
-    rclcpp::Subscription<visualization_msgs::msg::MarkerArray>::SharedPtr det_subscription_{nullptr};
+    rclcpp::Subscription<vision_msgs::msg::Detection3DArray>::SharedPtr det_subscription_{nullptr};
     rclcpp::Publisher<vision_msgs::msg::Detection2DArray>::SharedPtr publisher_{nullptr};
     rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr img_publisher_{nullptr}; 
-    rclcpp::Publisher<visualization_msgs::msg::MarkerArray>::SharedPtr pcl_publisher_{nullptr};
     std::shared_ptr<tf2_ros::TransformListener> transform_listener_{nullptr};
     std::unique_ptr<tf2_ros::Buffer> tf_buffer_{nullptr};
 
-    cv::Mat K = (cv::Mat_<double>(3,3) << 531.7944, 0, 296.3159, 0, 530.179, 255.4285, 0, 0, 1);
-    cv::Mat D = (cv::Mat_<double>(5,1) << -0.4512, 0.7176, -0.0014, 0.0017, -1.2896); // (k1,k2,p1,p2[,k3[,k4,k5,k6[,s1,s2,s3,s4[,τx,τy]]]])
+    cv::Mat K = cv::Mat(3, 3, CV_64F); 
+    cv::Mat D = cv::Mat(5, 1, CV_64F); // (k1,k2,p1,p2[,k3[,k4,k5,k6[,s1,s2,s3,s4[,τx,τy]]]])
 
     std::string output_frame;
     bool debug;
@@ -200,3 +199,4 @@ int main(int argc, char * argv[])
   rclcpp::shutdown();
   return 0;
 }
+
