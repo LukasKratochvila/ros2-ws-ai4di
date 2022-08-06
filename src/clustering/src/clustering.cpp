e06a370602c5e99aa139621a606aea7fc760363f
LukasKratochvila <kratochvila.lukas@gmail.com>
2022-08-06 11:43:39 +0200

    OBB fix
    Bugs fix
    Cleaning code

--- src/clustering/src/clustering.cpp
+++ src/clustering/src/clustering.cpp
@@ -20,14 +20,15 @@
 #include <pcl/segmentation/extract_clusters.h>
 #include <pcl/search/impl/search.hpp>
 
-#include <visualization_msgs/msg/marker.hpp>
-#include <visualization_msgs/msg/marker_array.hpp>
 #include <vision_msgs/msg/detection3_d.hpp>
 #include <vision_msgs/msg/detection3_d_array.hpp>
 
 #include <pcl/features/moment_of_inertia_estimation.h>
 
 #include <pcl/common/transforms.h>
+
+#include <math.h> 
+#define PI 3.14159265
 
 class Clustering : public rclcpp::Node
 {
@@ -54,17 +55,12 @@ public:
 
     subscription_ = this->create_subscription<sensor_msgs::msg::PointCloud2>(input_topic, 10, std::bind(&Clustering::topic_callback, this, std::placeholders::_1));
     detections_publisher_ = this->create_publisher<vision_msgs::msg::Detection3DArray>(output_topic, 10);
-
     RCLCPP_INFO(this->get_logger(), "Clustering node has started.");
   }
 
 private:
-  void Get_AABB_(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_ptr, pcl::Indices *indices, pcl::PointXYZ *center_point, pcl::PointXYZ *min_point, pcl::PointXYZ *max_point) const
-	{// https://pointclouds.org/documentation/moment__of__inertia__estimation_8hpp_source.html
-    center_point->x = 0.0f;
-    center_point->y = 0.0f;
-    center_point->z = 0.0f;
-    
+  void Get_AABB_(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_ptr, pcl::Indices *indices, pcl::PointXYZ *min_point, pcl::PointXYZ *max_point) const
+	{// https://pointclouds.org/documentation/moment__of__inertia__estimation_8hpp_source.html    
     min_point->x = std::numeric_limits<float>::max();
     min_point->y = std::numeric_limits<float>::max();
     min_point->z = std::numeric_limits<float>::max();
@@ -75,11 +71,7 @@ private:
     
     unsigned int number_of_points = static_cast <unsigned int>(indices->size());
     for (unsigned int i_point = 0; i_point < number_of_points; i_point++)
-    {
-      center_point->x += (*cloud_ptr)[(*indices)[i_point]].x;
-      center_point->y += (*cloud_ptr)[(*indices)[i_point]].y;
-      center_point->z += (*cloud_ptr)[(*indices)[i_point]].z;
-    
+    {    
       if ((*cloud_ptr)[(*indices)[i_point]].x <= min_point->x) min_point->x = (*cloud_ptr)[(*indices)[i_point]].x;
       if ((*cloud_ptr)[(*indices)[i_point]].y <= min_point->y) min_point->y = (*cloud_ptr)[(*indices)[i_point]].y;
       if ((*cloud_ptr)[(*indices)[i_point]].z <= min_point->z) min_point->z = (*cloud_ptr)[(*indices)[i_point]].z;
@@ -88,29 +80,23 @@ private:
       if ((*cloud_ptr)[(*indices)[i_point]].y >= max_point->y) max_point->y = (*cloud_ptr)[(*indices)[i_point]].y;
       if ((*cloud_ptr)[(*indices)[i_point]].z >= max_point->z) max_point->z = (*cloud_ptr)[(*indices)[i_point]].z;
     }
-    
-    if (number_of_points == 0)
-      number_of_points = 1;
-    
-    center_point->x /= number_of_points;
-    center_point->y /= number_of_points;
-    center_point->z /= number_of_points;
   }
   void topic_callback(const sensor_msgs::msg::PointCloud2::SharedPtr msg) const
   {
     pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
     pcl::fromROSMsg(*msg, *cloud);
+    std::stringstream log;
+
     vision_msgs::msg::Detection3DArray output_det;
     output_det.header = msg->header;
-
+    output_det.detections.clear();
+  
     // Dont have anything to process -> return
     if (!(*cloud).size())
     {
       detections_publisher_->publish(output_det);
       return;
     }
-
-    std::stringstream log;
     // Creating the KdTree object for the search method of the extraction
     pcl::search::KdTree<pcl::PointXYZ>::Ptr tree(new pcl::search::KdTree<pcl::PointXYZ>);
     tree->setInputCloud(cloud->makeShared());
@@ -130,40 +116,26 @@ private:
       RCLCPP_INFO(this->get_logger(), log.str());
       log.clear();
     }
-
     // Dont have anything to publish -> return
     if (!cluster_indices.size())
     {
       detections_publisher_->publish(output_det);
       return;
     }
-
     std::vector<double> cx, cy, cz, dx, dy, dz, ox, oy, oz, ow;
-
+    pcl::PointXYZ minPoint, maxPoint, center_point, min_point, max_point;
+  
     if (!OBB)
     {
-      std::vector<pcl::PointXYZ> center_list, size_list;
       for (auto &it : cluster_indices)
       {
-        pcl::PointXYZ centerPoint = {NAN, NAN, NAN};
-        pcl::PointXYZ minPoint = {NAN, NAN, NAN};
-        pcl::PointXYZ maxPoint = {NAN, NAN, NAN};
-
-        Get_AABB_(cloud, &it.indices, &centerPoint, &minPoint, &maxPoint);
-        center_list.emplace_back(centerPoint);
-        maxPoint.x -= minPoint.x;
-        maxPoint.y -= minPoint.y;
-        maxPoint.z -= minPoint.z;
-        size_list.emplace_back(maxPoint);
-      }
-      for (size_t i = 0; i < center_list.size(); i++)
-      {
-        cx.emplace_back(center_list.at(i).x);
-        cy.emplace_back(center_list.at(i).y);
-        cz.emplace_back(center_list.at(i).z);
-        dx.emplace_back(size_list.at(i).x);
-        dy.emplace_back(size_list.at(i).y);
-        dz.emplace_back(size_list.at(i).z);
+        Get_AABB_(cloud, &it.indices, &minPoint, &maxPoint);
+        cx.emplace_back((maxPoint.x + minPoint.x) / 2.0f);
+        cy.emplace_back((maxPoint.y + minPoint.y) / 2.0f);
+        cz.emplace_back((maxPoint.z + minPoint.z) / 2.0f);
+        dx.emplace_back(maxPoint.x);
+        dy.emplace_back(maxPoint.y);
+        dz.emplace_back(maxPoint.z);
         ox.emplace_back(0.0);
         oy.emplace_back(0.0);
         oz.emplace_back(0.0);
@@ -177,9 +149,7 @@ private:
         log << "Computing clusters OBB." << std::endl;
         RCLCPP_INFO(this->get_logger(), log.str());
         log.clear();
-      }
-      pcl::PointXYZ centerPoint, minPoint, maxPoint, center_point, min_point, max_point;
-      
+      }      
       for (pcl::PointIndices &it : cluster_indices)
       {
         pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_cluster(new pcl::PointCloud<pcl::PointXYZ>);
@@ -189,13 +159,15 @@ private:
         for (size_t i=0; i<cloud_cluster->size(); i++)
           cluster_idx.push_back(i);
 
-        Clustering::Get_AABB_(cloud_cluster, &cluster_idx, &centerPoint, &minPoint, &maxPoint);
+        Clustering::Get_AABB_(cloud_cluster, &cluster_idx, &minPoint, &maxPoint);
 
         double angle = 0;
         double surface_area = (maxPoint.x - minPoint.x) * (maxPoint.y - minPoint.y);
         min_point = minPoint;
         max_point = maxPoint;
-        center_point = centerPoint;
+        center_point.x = (maxPoint.x + minPoint.x) / 2.0f;
+        center_point.y = (maxPoint.y + minPoint.y) / 2.0f;
+        center_point.z = (maxPoint.z + minPoint.z) / 2.0f;
 
         // subract mean value for correct rotation
         for (const auto &idx : cluster_idx)
@@ -207,16 +179,16 @@ private:
 
         for(size_t angle_step=1; angle_step*step < 180; angle_step++){
           Eigen::Matrix4f transform_rot = Eigen::Matrix4f::Identity();
-          transform_rot.topLeftCorner(3, 3) = Eigen::Matrix3f(Eigen::Quaternionf(cos(step/2),sin(step/2),0,0));
+          transform_rot.topLeftCorner(3, 3) = Eigen::Matrix3f(Eigen::Quaternionf(cos(step/2/180.0f*PI),0,0,sin(step/2/180.0f*PI)));
 				  pcl::transformPointCloud(*cloud_cluster, *cloud_cluster, transform_rot);
 
-          Clustering::Get_AABB_(cloud_cluster, &cluster_idx, &centerPoint, &minPoint, &maxPoint);
+          Clustering::Get_AABB_(cloud_cluster, &cluster_idx, &minPoint, &maxPoint);
           if ((maxPoint.x - minPoint.x) * (maxPoint.y - minPoint.y) < surface_area){
             surface_area = (maxPoint.x - minPoint.x) * (maxPoint.y - minPoint.y);
             angle = angle_step*step;
             min_point = minPoint;
             max_point = maxPoint;
-          }
+          }          
         }
 
         cx.emplace_back(center_point.x);
@@ -225,10 +197,10 @@ private:
         dx.emplace_back(max_point.x - min_point.x);
         dy.emplace_back(max_point.y - min_point.y); 
         dz.emplace_back(max_point.z - min_point.z); 
-        ox.emplace_back(cos(angle/2));
-        oy.emplace_back(sin(angle/2));
-        oz.emplace_back(0);
-        ow.emplace_back(0);
+        ox.emplace_back(0.0);
+        oy.emplace_back(0.0);
+        oz.emplace_back(sin(-angle/2.0f/180.0f*PI));
+        ow.emplace_back(cos(-angle/2.0f/180.0f*PI));
       }
     }
 
