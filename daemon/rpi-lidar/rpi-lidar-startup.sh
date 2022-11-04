5725fc19d4b482f2709d78879fede7f1eb15ed1b
LukasKratochvila <kratochvila.lukas@gmail.com>
2022-11-04 12:19:18 +0100

    Update rpi-lidar-startup.sh
    Edited ptp setup

--- daemon/rpi-lidar/rpi-lidar-startup.sh
+++ daemon/rpi-lidar/rpi-lidar-startup.sh
@@ -2,10 +2,11 @@
 source /opt/ros/foxy/setup.bash
 source /home/ros/ros2-ws-ai4di/install/setup.bash
 
-echo "Setting up PTP"
-ptpd -i eth0 -M #& --foreground
+# ptp from nuke
+#echo "Setting up PTP"
+#ptpd -i eth0 -M #& --foreground
 
-sleep 12
+#sleep 12
 cd /home/ros/ros2-ws-ai4di/
 echo "Launching rpi-lidar"
 ros2 launch launch/Launch_rpi-lidar.py
